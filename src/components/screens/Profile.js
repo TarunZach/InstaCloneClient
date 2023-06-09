9bdf75882e12cc5661406fe488370cc09a597b58
TarunZach <tzacharias.97@gmail.com>
2023-06-09 12:30:10 +0530

    Profile page update

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,7 +2,37 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div style={{maxWidth:"550px",margin:"0px auto"}}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                        src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+                        alt="profiletest"
+                    />
+                </div>
+                <div>
+                    <h4>John Doe</h4>
+                    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+                        <h6>40 Posts</h6>
+                        <h6>40 Followers</h6>
+                        <h6>40 Following</h6>
+                    </div>
+                </div>  
+            </div>
+            <div className="gallery">
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+                <img className="item" alt="pfp" src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"/>
+            </div>
+        </div>
     )
 }
 
