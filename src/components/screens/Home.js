48dfb76ca72ae89d99d422cb97af26b1d2e7f882
TarunZach <tzacharias.97@gmail.com>
2023-06-20 22:04:31 +0530

    Like + Unlike test

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,23 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from '../../App';
 
-const Home = ()=>{
-    const [data,setData] = useState([])
-    useEffect(()=>{
-        fetch('/allpost',{
-            headers:{
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
+const Home = () => {
+    const [data, setData] = useState([])
+    const { state, dispatch } = useContext(UserContext)
+    useEffect(() => {
+        fetch('/allpost', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
             }
-        }).then(res=>res.json())
-        .then(result=>{
-            setData(result.posts)
-        })
-    },[])
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result)
+                setData(result.posts)
+            })
+    }, [])
 
-    return(
+    const likePost = (id) => {
+        fetch('/like', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                //   console.log(result)
+                const newData = data.map(item => {
+                    if (item._id === result._id) {
+                        return result
+                    } else {
+                        return item
+                    }
+                })
+                setData(newData)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+    const unlikePost = (id) => {
+        fetch('/unlike', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res => res.json())
+            .then(result => {
+                //   console.log(result)
+                const newData = data.map(item => {
+                    if (item._id === result._id) {
+                        return result
+                    } else {
+                        return item
+                    }
+                })
+                setData(newData)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    return (
         <div className="home">
             {
-                data.map(item=>{
-                    return(
+                data.map(item => {
+                    return (
                         <div className="card home-card" key={item._id}>
                             <h5>{item.postedBy.name}</h5>
                             <div className="card-image">
@@ -26,7 +80,31 @@ const Home = ()=>{
                                 />
                             </div>
                             <div className="card-content">
-                                <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                {item.likes.includes(state._id)
+                                    ?
+                                    <i
+                                        className="material-icons"
+                                        onClick={() => { unlikePost(item._id) }}
+                                    >
+                                        favorite_border
+                                    </i>
+                                    :
+                                    <i
+                                        className="material-icons"
+                                        onClick={() => { likePost(item._id) }}
+                                        style={{ color: "red" }}
+                                    >
+                                        favorite
+                                    </i>
+                                }
+                                <i
+                                    className="material-icons"
+                                    onClick={() => { unlikePost(item._id) }}
+                                >
+                                    favorite_border
+                                </i>
+
+                                <h6>{item.likes.length} Likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="Add a Comment" />
